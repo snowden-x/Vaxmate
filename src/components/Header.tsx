bd3d5b53f37daf9c79f83ba52b3207b1dde65dc9
Snowden <vonclaude06@gmail.com>
2024-09-17 16:47:53 +0000

    pushed mvp

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,12 +5,12 @@ import Link from 'next/link'
 export default function Header() {
 
     return (
-        <header className="rounded-lg top-0 sticky z-50 bg-white/50 shadow-md backdrop-blur-md border mx-20 py-0 text-secondary-foreground">
+        <header className="rounded-lg top-0 sticky z-50 bg-white/50 backdrop-blur-md  mx-20 py-0 text-secondary-foreground">
             <div className="container mx-auto px-4">
                 <div className="flex items-center justify-between py-3">
                     {/* Logo and App Name */}
                     <Link href="/">
-                        <div className="flex items-center space-x-2">
+                        <div className="flex items-center space-x-2 border-b-2 border-blue-500 border-dashed">
 
                             <span className="text-2xl font-bold text-green-500 bg-gradient-to-r from-blue-500 to-green-500 bg-clip-text text-transparent">vaxmate</span>
                         </div>
@@ -18,7 +18,7 @@ export default function Header() {
 
 
                     {/* User Profile */}
-                    <div className="hidden md:flex items-center space-x-4">
+                    <div className="hidden items-center space-x-4">
                         <Avatar>
                             <AvatarImage src="https://github.com/shadcn.png" alt="User" />
                             <AvatarFallback>JD</AvatarFallback>
